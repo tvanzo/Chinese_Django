0533f54cc72aef091d2d56ebdc2466a1691be962
tvanzo <tvanzo1@gmail.com>
2024-06-20 10:17:09 +0800

    updating settings for media

--- myproject/settings/base.py
+++ myproject/settings/base.py
@@ -141,7 +141,8 @@ MESSAGE_TAGS = {
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = env('MEDIA_ROOT', default=os.path.join(BASE_DIR, 'media'))
+MEDIA_ROOT = '/var/data/media'
+
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
