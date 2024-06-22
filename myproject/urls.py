ccb5cf5782e4b2813da3051739463a5127c98a08
tvanzo <tvanzo1@gmail.com>
2024-06-22 22:12:19 +0800

    removed settings.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -61,5 +61,4 @@ urlpatterns = [
          name='account_reset_password'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
