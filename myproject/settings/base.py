64a9e8b0ff18bb80b1c4222cfd9b7b51c3d7bd45
tvanzo <tvanzo1@gmail.com>
2024-07-12 15:07:13 +0800

    fixing media settings

--- myproject/settings/base.py
+++ myproject/settings/base.py
@@ -107,8 +107,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/data/media'
 
 # Static files finders
 STATICFILES_FINDERS = [
