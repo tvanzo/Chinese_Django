920c62ccb84dd9357770173d65cf7bec1a8d18bb
Tyler Vanzo <tylervanzo@Tylers-MacBook-Air.local>
2025-02-12 23:07:55 +0800

    elephant

--- myproject/settings/base.py
+++ myproject/settings/base.py
@@ -94,7 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+#
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 LANGUAGE_CODE = 'en-us'
