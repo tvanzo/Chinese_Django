3ca257f19e596af54d0dc9655f4794bd591ca5c7
Tyler Vanzo <tylervanzo@Tylers-MacBook-Air.local>
2025-03-02 12:44:38 +0800

    wsgi

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -4,3 +4,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings.production')
 
 application = get_wsgi_application()
+
+
+# Log Gunicorn settings at startup
+logger.info(f"Gunicorn command: {os.environ.get('GUNICORN_CMD_ARGS', 'Not set')}")
